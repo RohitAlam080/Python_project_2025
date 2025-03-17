2e42d73b6668b9c73b10a48ee274d2f8e76b3b14
Rohitalam080 <Rohitalam080123@gmail.com>
2025-03-17 21:23:18 +0530

    another QR_code of my Linkedin Profile

--- my_social_media_qr.py
+++ my_social_media_qr.py
@@ -18,5 +18,18 @@ gamer.save("github.png")
 
 print("congratulation!")
 
+data = "https://in.linkedin.com/in/rohit-alam-14828b338"
 
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=2,
+)
+qr.add_data(data)
+qr.make(fit=True)
 
+img =qr.make_image(fill_color="Black", back_color="White")
+img.save("linkedin.png")
+
+print("congratulation!")
